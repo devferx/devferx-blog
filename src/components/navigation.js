16cf4d0aece68fd0e59dca332977c774d2a2078f
Fernando <ferkki@hotmail.com>
2020-04-25 17:26:11 -0400

    twitter link added

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,17 @@
 import React from "react"
 // import {Link} from "gatsby"
+import {Helmet} from 'react-helmet'
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
-  <nav className="navigation"> 
+  <nav className="navigation">
     {/* <Link to="/contact">Contact</Link> */}
+    <Helmet>
+    <script defer src="https://kit.fontawesome.com/68499d0649.js" crossorigin="anonymous"></script>
+    </Helmet>
+    <a className="icon-link" href="https://twitter.com/devferx" target="__blank">
+      <i className="fab fa-twitter"></i>
+    </a>
     <ThemeChanger/>
   </nav>
 )
