ce21964bb0b264d6519211e2a6101d0aa01f77fe
devferx <37563836+devferx@users.noreply.github.com>
2020-05-10 13:11:43 -0400

    google analytics implemented

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-30027142-1",
+        trackingId: "UA-166022968-1",
         head: true,
       }
     },
